0c22fae5a2a7c8b16d17d01cac5259b4a61cbf73
Elizabete Pliene <144154063+elizabetepliene@users.noreply.github.com>
2024-02-26 09:31:42 +0000

    26.02.2024.

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-// 000RDB000 Jānis Programmētājs 1
+// 231RDC043 Elizabete Pliene RDCP0
 
 import java.util.Scanner;
 import java.io.FileReader;
@@ -8,23 +8,59 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("000RDB000 Jānis Programmētājs 1. grupa");
-		
+		System.out.println("231RDC043 Elizabete Pliene RDCP0 grupa");
 		
 		String fileName;
 		System.out.println("input file name:");
 		fileName = sc.nextLine();
 		sc.close();
-		
-		// nolasīt failu
-		
-		// ievadīto no tastatūras faila nosaukumu fileName nemodificēt, 
-		// nepievienot mapes vārdu, nepievienot tipu txt
-		
-		System.out.println("result:");
-		
-		// izvadīt rezultātus	
+		FileReader fr = new FileReader(fileName);
+		sc = new Scanner(fr);
+		String s;
+		String result;
+		Double videja=0.0;
+		int paradi = 0;
+		int count = 0;
+		System.out.println("result: ");
+		while(sc.hasNextLine()){
+			s = sc.nextLine();
+			count = 0;
+			videja = 0.0;
+			paradi = 0;
+			result = "";
+			for (int i = 0; i < s.length(); i++){
+				char ch = s.charAt(i);
+				if(Character.isDigit(ch)){
+					videja = videja+Character.getNumericValue(ch);
+					if(Character.getNumericValue(ch)<4){
+						paradi++;
+					}  
+					count++;
+				}
+			}
+			videja= videja/count;
+			if(videja <=5){
+				for(int i =0;i<s.length();i++){
+					char ch = s.charAt(i);
+					if(Character.isLetter(ch)){
+						if(Character.isLowerCase(ch)){
+							result = result+ch;
+						}
+						else
+						result=result+" "+ch;
+					}
 
-	}
+					}
+					}
 
-}
+			if(paradi>0&&result!=""){
+				System.out.println(result.trim()+" "+paradi);
+			}
+			}
+				
+			sc.close();
+			}
+
+			
+		}
+
