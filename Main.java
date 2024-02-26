6744196aee345d77373da5856e4c649656da412e
Elizabete Pliene <144154063+elizabetepliene@users.noreply.github.com>
2024-02-26 10:07:43 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,30 @@
+// 000RDB000 Jānis Programmētājs 1
+
+import java.util.Scanner;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("000RDB000 Jānis Programmētājs 1. grupa");
+		
+		
+		String fileName;
+		System.out.println("input file name:");
+		fileName = sc.nextLine();
+		sc.close();
+		
+		// nolasīt failu
+		
+		// ievadīto no tastatūras faila nosaukumu fileName nemodificēt, 
+		// nepievienot mapes vārdu, nepievienot tipu txt
+		
+		System.out.println("result:");
+		
+		// izvadīt rezultātus	
+
+	}
+
+}
